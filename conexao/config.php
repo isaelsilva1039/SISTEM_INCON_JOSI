9c410fd2347343ace25dba04087c67d7d26dabb7
isaelsilva1039 <isaelsilva1039@gmail.com>
2021-09-25 19:35:07 -0300

    ll

--- conexao/config.php
+++ conexao/config.php
@@ -1,37 +1,36 @@
 <?php 
 
- class  Configura{
+class  Configura{
     
     // nome banco 
-    public static function banco (){
-        //$banco = "BI_LOGISTICA";
-        //$banco = "postgres";
-        $banco = "u119780054_BI_LOGISTICA";
-        return $banco;
-    }
-    // host do banco 
+
     public static function host (){
         // $host = "192.168.87.250";
         // $host="localhost";
-           $host = "localhost";
+        //    $host = "localhost";
+           $host = "185.201.10.73";
         return $host;
     }
 
     // usuario do banco
-    public static function usuario(){
+
+	 public static function usuario(){
         // $usuario = "logistica";
         // $usuario = "ROOT"
         // $usuario = "postgres";
-        $usuario = "root";
+        // $usuario = "root";
+        $usuario = "u119780054_logistica";
         return $usuario;
 
     }
 
     // senha do banco 
+	 
     public static function senha (){
         // $senha = "logisticabls";
         //$senha = 123;
-        $senha = "";
+        // $senha = "";
+        $senha = "Isaelsilva@1039";
 
         // Isaelsilva@1039   senha do banco de dados  MYSQL produção 
         return $senha;
@@ -40,14 +39,6 @@
     public static function dns(){
         // $dns = "pgsql";
         $dns = "mysql";
-        return $dns;
-    }
-    
-    public static function porta(){
         // $porta = '5432';
         // return $porta;
     }
-
-
-}
-
